940ecf5f952967600fce5f61a367d72d87032fff
Vijay-Thumar <vijaythumar1020@gmail.com>
2022-04-02 13:34:04 +0530

    navigation bar

---
+++ sarathi-frontend/src/components/HomePage.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './HomePage.css'
+
+
+function Homepage() {
+    return (
+        <div>
+            <div className='nav_container'>
+                <div className='navbar-brand'>Sarthik Logo</div>
+                <div className='nav_mid_iteams'>
+                    <div className=''>HOME</div>
+                    <div>ABOUT</div>
+                    <div>TEAM</div>
+                    <div>SERVICES</div>
+                </div>
+                <div><button className='btn btn-outline-dark'>LOGIN</button></div>
+            </div>
+
+            <div>
+                <div className='map_container'>
+                </div>
+                <div className='signup_button'>
+                    <button className='btn-secondary text-light btn-outline-dark'>SIGN UP As Vendor</button>
+                    <button className='btn-secondary text-light btn-outline-dark'>SIGN UP As Customer</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Homepage
