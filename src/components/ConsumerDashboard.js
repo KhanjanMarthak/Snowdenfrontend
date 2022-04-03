7223924a9785fb66d4667689e3775cb85afea188
Vijay-Thumar <vijaythumar1020@gmail.com>
2022-04-03 10:53:26 +0530

    added particle and user deshbord

--- src/components/ConsumerDashboard.js
+++ src/components/ConsumerDashboard.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import mainlogo from '../assets/sarathi.png';
+import profile from '../assets/user.png';
 import "./ConsumerDashboard.css";
+import favorite from '../assets/love.png'
 
 function ConsumerDashboard() {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <span className="navbar-brand"><img className='mainlogo' src={mainlogo} alt='sarthik' /> Sarthik</span>
+                <span className="navbar-brand"><img className='mainlogo' src={mainlogo} alt='sarthi' /> Sarthi</span>
 
                 <div className="form-inline my-2 my-lg-0">
-                    <button className="btn btn-outline-success my-2 my-sm-0 my_btn1" type="submit">LOGIN</button>
+                    <div className="btn  my-2 my-sm-0 my_btn1" type="submit">
+                        <img className="profilephoto" src={profile} alt="myimage" />
+                    </div>
+                    <div className="btn  my-2 my-sm-0 my_btn1" type="submit">
+                        <img className="profilephoto" src={favorite} alt="myimage" />
+                    </div>
+                    <button className="btn btn-outline-success my-2 my-sm-0 my_btn1" type="submit">Logout</button>
                 </div>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -35,13 +43,13 @@ function ConsumerDashboard() {
                         </ul>
                     </div>
                     <div className="form-inline my-2 my-lg-0">
-                        <button className="btn btn-outline-success my-2 my-sm-0 my_btn" type="submit">
-                            <img src="./assets/user.png" alt="myimage"/>
-                        </button>
-                        <button className="btn btn-outline-success my-2 my-sm-0 my_btn" type="submit">
-                            <img src="./img/google.png" alt="myimage"/>
-                        </button>
-                        <button className="btn btn-outline-success my-2 my-sm-0 my_btn" type="submit">LOGIN</button>
+                        <div className="btn  my-2 my-sm-0 my_btn" type="submit">
+                            <img className="profilephoto" src={profile} alt="myimage" />
+                        </div>
+                        <div className="btn  my-2 my-sm-0 my_btn" type="submit">
+                            <img className="profilephoto" src={favorite} alt="myimage" />
+                        </div>
+                        <button className="btn btn-outline-success my-2 my-sm-0 my_btn" type="submit">Logout</button>
                     </div>
                 </div>
             </nav>
